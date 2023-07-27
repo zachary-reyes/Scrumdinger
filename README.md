d72e8f9098e1368bf684aa9ca3281fbf767871ff
ZARface <zacharyaustin@outlook.com>
2023-07-27 14:41:40 -0700

    created ScrumsView (app's root list view), made DailyScrum Identifiable

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## IOS App Dev Tutorial
+
+Link: https://training.apple.com/appdeveloper
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,24 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Zachary Reyes on 7/27/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
