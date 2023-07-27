81c4e764f90e6e3d9af545ab270c8ab04e6ffa0c
ZARface <zacharyaustin@outlook.com>
2023-07-27 11:32:20 -0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Zachary Reyes on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
